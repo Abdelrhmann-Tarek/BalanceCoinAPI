5fb825441f2d76687703dc4573630a652f76729f
Abdo Tarek <abdelrahmantarek696@gmail.com>
2025-04-28 17:17:38 +0200

    create IExpenseService

---
+++ BalanceCoinAPI/BalanceCoinAPI/Application/Services/IExpenseService.cs
@@ -0,0 +1,18 @@
+﻿using BalanceCoinAPI.DTOs;
+
+namespace BalanceCoinAPI.Application.Services
+{
+    public interface IExpenseService
+    {
+        Task<ExpenceDTO> CreateExpenseAsync(ExpenceDTO expenseDto);//Create a new expense/income entry
+
+        Task<List<ExpenceDTO>> GetAllExpensesAsync();//Retrieve a list of all expenses/incomes.
+
+        Task<ExpenceDTO?> GetExpenseByIdAsync(int id); //Retrieve details of a specific expense/income by ID.
+        Task<List<ExpenceDTO>> GetExpenseByCategoryAsync(int categoryId); //Filter expenses/income by category
+
+        Task<ExpenceDTO?> UpdateExpenseAsync(int id,ExpenceDTO incomeDto);//Update an existing expense/income entry.
+
+        Task<bool> DeleteExpenseAsync(int id);//Delete an expense/income entry.
+    }
+}
