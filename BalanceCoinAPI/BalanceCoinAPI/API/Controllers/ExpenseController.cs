9adbe839e5590b6ac797f490b9c310c05175bb49
Abdo Tarek <abdelrahmantarek696@gmail.com>
2025-04-29 15:24:56 +0200

    creating ExpenseController &ExpenseController.GetAll

--- BalanceCoinAPI/BalanceCoinAPI/API/Controllers/ExpenseController.cs
+++ BalanceCoinAPI/BalanceCoinAPI/API/Controllers/ExpenseController.cs
@@ -1,6 +1,23 @@
-﻿namespace BalanceCoinAPI.API.Controllers
+﻿using BalanceCoinAPI.DTOs;
+using BalanceCoinAPI.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BalanceCoinAPI.API.Controllers
 {
-    public class ExpenseController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExpenseController : ControllerBase
     {
+        private readonly IExpenseService _expenseService;
+        public ExpenseController(IExpenseService expenseService)
+        {
+            expenseService = _expenseService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<ExpenceDTO>> GetAll()
+        {
+            var expense = await _expenseService.GetAllExpensesAsync();
+            return Ok(expense);
+        }
     }
 }
